0afd57db727a512e9324af4befaa586459715448
hung3009 <nguyen.vu.hung309@gmail.com>
2023-09-18 11:31:12 +0700

    wfe

--- server/server.ts
+++ server/server.ts
@@ -1,7 +1,10 @@
 import { app } from "./app";
+import connectDB from "./utils/db";
+import User from "./models/user.model"
 require("dotenv"). config () ;
 
 // create server
 app.listen (process.env. PORT, () => {
     console.log(`Server is connected with port ${process.env.PORT}`);
+    connectDB();
 });
