5f6e282f5db8c825d43dc13c9f456618571fd684
NguyenHung <nguyenhung@NguyenHungs-Air.lan>
2023-09-21 16:05:10 +0700

    almost done server

--- server/app.ts
+++ server/app.ts
@@ -7,7 +7,10 @@ import { ErrorMiddleware } from "./middleware/error";
 import userRouter from "./routes/user.route";
 import courseRouter from "./routes/course.route";
 import orderRouter from "./routes/order.route";
-import notificationRouter from "./routes/notification.route"
+import notificationRouter from "./routes/notification.route";
+import analyticsRouter from "./routes/analytics.route";
+import layoutRouter from "./routes/layout.route";
+
 
 // body parser
 app.use(express.json({ limit: "50mb" }));
@@ -23,7 +26,7 @@ app.use(
 );
 
 // routes
-app.use("/api/v1", userRouter, orderRouter,courseRouter,notificationRouter);
+app.use("/api/v1", userRouter, orderRouter,courseRouter,notificationRouter,analyticsRouter,layoutRouter);
 
 
 
