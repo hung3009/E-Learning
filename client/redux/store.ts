78c8fb9e831fec336df39c5556a17f49ec76314f
NguyenHung <nguyenhung@NguyenHungs-Air.lan>
2023-09-27 11:53:40 +0700

    x

---
+++ client/redux/store.ts
@@ -0,0 +1,12 @@
+"use client";
+import { configureStore } from "@reduxjs/toolkit";
+
+
+export const store = configureStore({
+  reducer: {
+    
+  },
+  devTools: false,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(),
+});
