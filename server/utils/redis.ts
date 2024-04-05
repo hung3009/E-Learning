99f299536744e3ceaf01a979a5ed6ceceeb93f0b
nguyenhung <nguyen.vu.hung309@gmail.com>
2024-04-05 23:40:23 +0700

    Fix Redis Config

--- server/utils/redis.ts
+++ server/utils/redis.ts
@@ -1,12 +1,22 @@
-import {Redis} from 'ioredis'
-require('dotenv').config();
+// import {Redis} from 'ioredis'
+// require('dotenv').config();
 
-const redisClient = () => {
-    if(process.env.REDIS_URL){
-        console.log(`Redis connected`)
-        return process.env.REDIS_URL;
-    }
-    throw new Error('Redis connection fail')
-}
+// const redisClient = () => {
+//     if(process.env.REDIS_URL){
+//         console.log(`Redis connected`)
+//         return process.env.REDIS_URL;
+//     }
+//     throw new Error('Redis connection fail')
+// }
 
-export const redis = new Redis(redisClient());
+// export const redis = new Redis(redisClient());
+
+import { Redis } from '@upstash/redis'
+
+const redis = new Redis({
+  url: 'https://firm-bull-44495.upstash.io',
+  token: 'Aa3PASQgYTlmMWIyNjAtMDIyNy00MzhmLTk2NGQtODdjODVlMzBiYjQ1YjRiNjI1ZGU1YjhhNDczY2E0MmZiNmEwODQzYzg1MGQ=',
+})
+
+await redis.set('foo', 'bar');
+const data = await redis.get('foo');
